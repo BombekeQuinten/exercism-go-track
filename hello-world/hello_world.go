b643f27e0c61b596c4787bacf8d5638778d43643
Quinten Bombeke <BombekeQuinten@users.noreply.github.com>
2018-11-14 18:03:34 +0100

    Added Hello World

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting has a function that returns a string "Hello, world!"
+package greeting
+
+// Returns string "Hello, world!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
